477a6ea4aed242224a9c02eef06658f378981bac
Cheikh  Fall <cf9769106@gmail.com>
2023-03-11 13:25:58 +0000

    first commit

--- core/abstrait/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-class AbstractSerializer(serializers.ModelSerializer):
-    # On définit la méthode de sérialisation pour l'identifiant unique de l'utilisateur
-    id = serializers.UUIDField(source='public_id', read_only=True, format='hex')
-    # On définit les champs 'created' et 'updated' en lecture seule
-    created = serializers.DateTimeField(read_only=True)
-    updated = serializers.DateTimeField(read_only=True)
-    
--- tests.py
+++
@@ -1,4 +0,0 @@
-def add(a, b):
- return a + b
-def test_sum():
-    assert sum([1, 2]) == 3
